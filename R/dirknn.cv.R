50c32b5877251f40d2f00adc5fd91a89f0c26f9b
Michail Tsagris <mtsagris@yahoo.gr>
2020-03-24 09:14:33 +0200

    dirknn.cv

--- R/dirknn.cv.R
+++ R/dirknn.cv.R
@@ -24,31 +24,38 @@ stratified = TRUE, seed = FALSE, parallel = FALSE, pred.ret = FALSE) {
 }
 
 
-makefolds <- function(target, nfolds = 10, stratified = TRUE, seed = FALSE) {
+
+makefolds <- function(ina, nfolds = 10, stratified = TRUE, seed = FALSE) {
   names <- paste("Fold", 1:nfolds)
   runs <- sapply(names, function(x) NULL)
   if (seed)  set.seed(1234)
 
   if ( !stratified ) {
     oop <- options(warn = -1)
-    on.exit( options(oop) )
-    mat <- matrix(sample(length(target)), ncol = nfolds)
-    for (i in 1:c(nfolds - 1)) runs[[i]] <- mat[, i]
-    names <- prod(dim(mat)) - length(target)
-    runs[[nfolds]] <- mat[1:c(nrow(mat) - names), nfolds]
+    on.exit(options(oop))
+    ep <- sample( length(ina) )
+    nr <- round( length(ina)/nfolds )
+    mat <- matrix( ep[1:(nr * nfolds) ], ncol = nfolds )
+    for ( i in 1:nfolds ) runs[[ i ]] <- mat[, i]
+    rem <- ep[ - c(1:(nr * nfolds)) ]
+    ela <- sample(nfolds, length(rem))
+    if ( length(ela) > 0 )  for ( i in 1:length(ela) )  runs[[ ela[i] ]] <- c( runs[[ i ]], rem[ i ] )
   } else {
-    labs <- unique(target)
+    labs <- unique(ina)
     run <- list()
     for (i in 1:length(labs)) {
-      names <- which(target == labs[i])
+      names <- which( ina == labs[i] )
       run[[i]] <- sample(names)
     }
     run <- unlist(run)
-    for (i in 1:length(target)) {
+    for ( i in 1:length(ina) ) {
       k <- i %% nfolds
-      if (k == 0)  k <- nfolds
-      runs[[k]] <- c(runs[[k]], run[i])
+      if ( k == 0 )  k <- nfolds
+      runs[[k]] <- c( runs[[ k ]], run[i] )
     }
   }
+  for (i in 1:nfolds)  {
+    if ( any( is.na(runs[[ i ]]) ) )  runs[[ i ]] <- runs[[ i ]][ !is.na(runs[[ i ]]) ]
+  }
   runs
-}
+} 
