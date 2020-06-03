438536d1a21e005259ac9781a1c4bb8df0a4cff6
statlink <mtsagris@yahoo.gr>
2020-06-03 11:37:47 +0300

    changes

--- R/col_mle.R
+++ R/col_mle.R
@@ -106,10 +106,11 @@ collindley.mle <- function(x) {
 colnormal.mle <- function(x) {
   n <- dim(x)[1]
   m <- Rfast::colmeans(x)
-  s <- (Rfast::colsums(x^2) - n * m^2)/(n - 1)
-  loglik <-  - 0.5 * n * ( log(2 * pi) + log(s) ) - 0.5 * (n - 1)
-  res <- cbind(m, s, loglik)
-  colnames(res) <- c("mean", "unbiased variance", "log-likelihood")
+  s <- Rfast::colVars(x)
+  ss <- s * (n - 1) / n
+  loglik <-  - 0.5 * n * ( log(2 * pi) + log(ss) ) - 0.5 * n
+  res <- cbind(m, ss, s, loglik)
+  colnames(res) <- c("mean", "biased variance", "unbiased variance", "log-likelihood")
   res
 }
 
