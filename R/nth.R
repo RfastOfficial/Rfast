7cc8cdcd6a2c97f248491a2dee97d4cd1d5a10be
Manos Papadakis <papadakm95@gmail.com>
2020-06-26 23:54:31 +0300

    update

--- R/nth.R
+++ R/nth.R
@@ -1,26 +1,26 @@
 #[export]
-rownth <- function(x,elems,descending=FALSE,na.rm = FALSE,index.return = FALSE,parallel = FALSE) {
+rownth <- function(x,elems,num.of.nths=1,descending=FALSE,na.rm = FALSE,index.return = FALSE,parallel = FALSE) {
 	if(parallel){
-		.Call(Rfast_row_nth_p,x,elems,descending,na.rm,index.return)
+		.Call(Rfast_row_nth_p,x,elems,num.of.nths,descending,na.rm,index.return)
 	}else{
-  		.Call(Rfast_row_nth,x,elems,descending,na.rm,index.return)
+  		.Call(Rfast_row_nth,x,elems,num.of.nths,descending,na.rm,index.return)
 	}
 }
 
 #[export]
-colnth <- function(x,elems,descending=FALSE,na.rm = FALSE,index.return = FALSE,parallel = FALSE) {
+colnth <- function(x,elems,num.of.nths=1,descending=FALSE,na.rm = FALSE,index.return = FALSE,parallel = FALSE) {
 	if(parallel){
-  		.Call(Rfast_col_nth_p,x,elems,descending,na.rm,index.return)
+  		.Call(Rfast_col_nth_p,x,elems,num.of.nths,descending,na.rm,index.return)
 	}else{
-		.Call(Rfast_col_nth,x,elems,descending,na.rm,index.return)
+		.Call(Rfast_col_nth,x,elems,num.of.nths,descending,na.rm,index.return)
 	}
 }
 
 #[export]
-nth <- function(x,k,descending=FALSE,index.return=FALSE,na.rm = FALSE) {
+nth <- function(x,k,num.of.nths=1,descending=FALSE,index.return=FALSE,na.rm = FALSE) {
 	if(is.integer(x)){
 		.Call(Rfast_nth_int,x,k)
 	}else{
-  		.Call(Rfast_nth,x,k,descending,na.rm,index.return)
+  		.Call(Rfast_nth,x,k,num.of.nths,descending,na.rm,index.return)
   	}
 }
