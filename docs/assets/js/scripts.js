a563fd4169938f018c08003f15ea1fd8d45f47b7
Manos Papadakis <papadakm95@gmail.com>
2020-09-10 18:32:39 +0300

    update footer

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,36 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Scroll link
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), 0);
+	});
+	
+    /*
+        Background slideshow
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    $('footer').backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+	
+});
