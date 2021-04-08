6025432d27de8c4f5ca1f4ae88f244efee35c5c2
Manos Papadakis <papadakm95@gmail.com>
2021-04-08 15:56:25 +0300

    fix bug about col and row names

--- R/data.frame.to_matrix.R
+++ R/data.frame.to_matrix.R
@@ -1,15 +1,15 @@
 #[export]
 data.frame.to_matrix <- function(x,col.names = NULL,row.names = NULL) {
-	if(is.null(col.names) && is.null(row.names)){
-		x <- .Call("Rfast_frame_to_matrix",x)
-	}else{
-		y <- .Call("Rfast_frame_to_matrix",x)
-		if(!is.null(col.names) && col.names == TRUE){
-			colnames(y) <- colnames(x)
-		}
-		if(!is.null(row.names) && row.names == TRUE){
-			rownames(y) <- rownames(x)
-		}
+	y <- .Call("Rfast_frame_to_matrix",x)
+	if(is.character(col.names) && length(col.names)==ncol(y)){
+		colnames(y) <- col.names
+	}else if(col.names == TRUE){
+		colnames(y) <- colnames(x)
 	}
-	x
+	if(is.character(row.names) && length(row.names)==nrow(y)){
+		rownames(y) <- row.names
+	}else if(col.names == TRUE){
+		rownames(y) <- rownames(x)
+	}
+	y
 }
