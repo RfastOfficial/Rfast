bc8e4acadecdd520d023f7c19ecb1f7298245c5c
Manos Papadakis <papadakm95@gmail.com>
2021-04-19 17:34:19 +0300

    fix bug with names

--- R/data.frame.to_matrix.R
+++ R/data.frame.to_matrix.R
@@ -3,12 +3,12 @@ data.frame.to_matrix <- function(x,col.names = NULL,row.names = NULL) {
 	y <- .Call("Rfast_frame_to_matrix",x)
 	if(is.character(col.names) && length(col.names)==ncol(y)){
 		colnames(y) <- col.names
-	}else if(col.names == TRUE){
+	}else if(is.logical(col.names) && col.names == TRUE){
 		colnames(y) <- colnames(x)
 	}
 	if(is.character(row.names) && length(row.names)==nrow(y)){
 		rownames(y) <- row.names
-	}else if(col.names == TRUE){
+	}else if(is.logical(row.names) && row.names == TRUE){
 		rownames(y) <- rownames(x)
 	}
 	y
