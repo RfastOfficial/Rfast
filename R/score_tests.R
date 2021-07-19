f37b62e27ddd50fdf5f75b57a37802b76e40b594
statlink <mtsagris@yahoo.gr>
2021-07-19 23:23:12 +0300

    change

--- R/score_tests.R
+++ R/score_tests.R
@@ -15,7 +15,7 @@ score.betaregs <- function (y, x, logged = FALSE) {
 #[export]
 score.expregs <- function(y, x, logged = FALSE) {
   lam <- mean(y)
-  u <- Rfast::colsums(x * y) * lam - Rfast::colsums(x) 
+  u <- Rfast::eachcol.apply(x, y) * lam - Rfast::colsums(x) 
   vu <- Rfast::colsums(x^2) * lam^4
   stat <- u^2 / vu
   pvalue <- pchisq(stat, 1, lower.tail = FALSE, log.p = logged)
