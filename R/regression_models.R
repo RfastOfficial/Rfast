07175f5078a1297792aa1327968152fb5a877fd5
statlink <mtsagris@yahoo.gr>
2021-09-05 01:53:00 +0300

    change

--- R/regression_models.R
+++ R/regression_models.R
@@ -176,7 +176,7 @@ lmfit <- function(x, y, w = NULL) {
   if ( is.null(w) ) {
     be <- solve( crossprod(x), crossprod(x, y) )
   } else  be <- solve( crossprod(x, w * x), crossprod(x, w * y) )
-  e <- y - x %*% be 
+  e <- as.vector( y - x %*% be )
   rownames(be) <- colnames(x)
   list(be = be, residuals = e)
 }
