a4e9bbc1e221e5d28538fd91a41766ec172950cf
Manos Papadakis <papadakm95@gmail.com>
2021-11-09 16:32:27 +0200

    fix big in nth using na remove

--- inst/include/Rfast/templates.h
+++ inst/include/Rfast/templates.h
@@ -1056,12 +1056,10 @@ double nth_simple(T& x,const int& elem,const bool& descend){
 template<class T>
 double nth_na_rm(T& x,const int& elem,const bool& descend){
     const int new_end=remove_if(x.begin(),x.end(),R_IsNA)-x.begin();
-    if(elem<new_end){
-	    descend ?
-	    nth_element(x.begin(),x.begin()+elem-1,x.begin()+new_end,[&](double a,double b){return a>b;})
-	    :
-	    nth_element(x.begin(),x.begin()+elem-1,x.begin()+new_end);
-    }
+    descend ?
+    nth_element(x.begin(),x.begin()+elem-1,x.begin()+new_end,[&](double a,double b){return a>b;})
+    :
+    nth_element(x.begin(),x.begin()+elem-1,x.begin()+new_end);
     return x[elem-1];
 }
 
