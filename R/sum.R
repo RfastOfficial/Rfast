6bff2bc2bec34187cb09e9fb3684349de93641bf
Manos Papadakis <papadakm95@gmail.com>
2021-11-11 18:40:41 +0200

    Update sum.R

--- R/sum.R
+++ R/sum.R
@@ -1,17 +1,17 @@
 #[export]
-colsums <- function(x,indices = NULL,parallel = FALSE) {
+colsums <- function(x,indices = NULL,parallel = FALSE,na.rm = FALSE) {
 	if(parallel){
 		.Call(Rfast_col_sums_p,x)
 	}else{
-  		.Call(Rfast_col_sums,x,indices)
+  		.Call(Rfast_col_sums,x,indices,na.rm)
 	}
 }
 
 #[export]
-rowsums <- function(x,indices = NULL,parallel = FALSE) {
+rowsums <- function(x,indices = NULL,parallel = FALSE,na.rm = FALSE) {
 	if(parallel){
   		.Call(Rfast_row_sums_p,x)
   	}else{
-  		.Call(Rfast_row_sums,x,indices)
+  		.Call(Rfast_row_sums,x,indices,na.rm)
   	}
 }
