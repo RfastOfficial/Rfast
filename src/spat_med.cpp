93da3a7ebc749081938e5c1f9207f0c65b6d452a
Manos Papadakis <papadakm95@gmail.com>
2022-05-30 14:06:41 +0300

    Update spat_med.cpp

--- src/spat_med.cpp
+++ src/spat_med.cpp
@@ -6,31 +6,30 @@
 using namespace arma;
 using namespace Rcpp;
 
-rowvec spat_med(NumericMatrix Y,const double tol = 0.000000001){
-    mat y = mat(Y.begin(), Y.nrow(), Y.ncol(), false); 
-    colvec ww;
-    rowvec u1,u2,wei;
-    mat z;
-    uvec ind;
-
-    u1=colMedians(y);
-    z=y.each_row()-u1;
-    ww =1/sqrt(sum(square(z),1));
-    ind = find_finite(ww);
-    y=y.rows(ind);
-    z=z.rows(ind);
-    wei=ww/sum(ww);
-    u2=wei*y;
-    while(sum(abs(u2-u1))>tol){
-        z=y.each_row()-u2;
-        u1=u2;
-        ww=1/sqrt(sum(square(z),1));
-        if(is_finite(ww.max())){
-            wei=ww/sum(ww);
-            u2=wei*y;
-        }
+rowvec spat_med(NumericMatrix Y,const double tol = 1e-09){
+  mat y = mat(Y.begin(), Y.nrow(), Y.ncol(), false); 
+  mat z(y.n_rows,y.n_cols);
+  colvec ww(z.n_rows);
+  rowvec u1(y.n_rows),u2;
+  uvec ind;
+  
+  u1=colMedians(y);
+  z=y.each_row()-u1;
+  ww =1/sqrt(sum(square(z),1));
+  ind = find_finite(ww);
+  y=y.rows(ind);
+  z=z.rows(ind);
+  ww=ww(ind);
+  u2=(ww/accu(ww))*y;
+  while(sum(abs(u2-u1))>tol){
+    z=y.each_row()-u2;
+    u1=u2;
+    ww=1/sqrt(sum(square(z),1));
+    if(is_finite(ww.max())){
+      u2=(ww/accu(ww))*y;
     }
-    return u2;
+  }
+  return u2;
 }
 
 
