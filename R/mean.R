5a8ad3aa2ea520c37bddb2e2ce416e24ed11f3b3
Manos Papadakis <papadakm95@gmail.com>
2023-01-18 15:39:54 +0200

    Update mean.R

--- R/mean.R
+++ R/mean.R
@@ -1,10 +1,6 @@
 #[export]
 colmeans <- function(x,parallel = FALSE) {
-	if(parallel){
-		.Call(Rfast_col_mean_p,x)
-	}else{
-  		as.vector(.Call(Rfast_col_means,x))
-  	}
+	.Call(Rfast_col_means,x,parallel)
 }
 
 #[export]
