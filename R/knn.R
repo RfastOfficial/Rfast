fdcb50fc3dd2c2e52831de51cac75e2e8066b862
statlink <mtsagris@yahoo.gr>
2023-01-19 15:16:09 +0200

    change

--- R/knn.R
+++ R/knn.R
@@ -12,44 +12,43 @@ knn.cv <- function(folds = NULL, nfolds = 10, stratified = FALSE, seed = NULL, y
   .Call(Rfast_k_nn_cv,folds, y, x, k, dist.type, type, method, freq.option, pred.ret, mem.eff)
 }
 
-generateFolds <- function(target, nfolds = 10, stratified = TRUE, seed = NULL) {
-  names <- paste("Fold", 1:nfolds)
-  runs <- sapply(names, function(x) NULL)
-  if ( is.null(seed) )  set.seed(seed)
-
-  if (!stratified) {
+generateFolds <- function (ina, nfolds = 10, stratified = TRUE, seed = NULL) {
+    names <- paste("Fold", 1:nfolds)
+    runs <- sapply(names, function(x) NULL)
+    if (!is.null(seed)) 
+        set.seed(seed)
     oop <- options(warn = -1)
-    on.exit( options(oop) )
-    ep <- sample(length(target))
-    nr <- round(length(target) / nfolds)
-    mat <- matrix(ep[1:(nr * nfolds)], ncol = nfolds)
-    for (i in 1:nfolds) runs[[i]] <- mat[, i]
-    rem <- ep[-c(1:(nr * nfolds))]
-    ela <- sample(nfolds, length(rem))
-    if (length(ela) > 0) {
-        for (i in 1:length(ela)) {
-            runs[[ela[i]]] <- c(runs[[i]], rem[i] )
+    on.exit(options(oop))
+    if (!stratified) {
+        rat <- length(ina)%%nfolds
+        mat <- matrix(Rfast2::Sample.int(length(ina)), ncol = nfolds)
+        mat[-c(1:length(ina))] <- NA
+        for (i in 1:c(nfolds - 1)) runs[[i]] <- mat[, i]
+        a <- prod(dim(mat)) - length(ina)
+        runs[[nfolds]] <- mat[1:c(nrow(mat) - a), nfolds]
+    }
+    else {
+        labs <- unique(ina)
+        run <- list()
+        for (i in 1:length(labs)) {
+            names <- which(ina == labs[i])
+            run[[i]] <- sample(names)
+        }
+        run <- unlist(run)
+        for (i in 1:length(ina)) {
+            k <- i%%nfolds
+            if (k == 0) 
+                k <- nfolds
+            runs[[k]] <- c(runs[[k]], run[i])
         }
     }
-  } else {
-    labs <- unique(target)
-    run <- list()
-    for (i in 1:length(labs)) {
-      names <- which(target == labs[i])
-      run[[i]] <- sample(names)
+    for (i in 1:nfolds) {
+        if (any(is.na(runs[[i]]))) 
+            runs[[i]] <- runs[[i]][!is.na(runs[[i]])]
     }
-    run <- unlist(run)
-    for (i in 1:length(target)) {
-      k <- i %% nfolds
-      if (k == 0)  k <- nfolds
-      runs[[k]] <- c(runs[[k]], run[i])
-    }
-  }
-  for (i in 1:nfolds)  {
-    if (any(is.na(runs[[i]]))) {
-        runs[[i]] <- runs[[i]][!is.na(runs[[i]])]
-    }
-  }
-  runs
+    if (length(runs[[nfolds]]) == 0) 
+        runs[[nfolds]] <- NULL
+    runs
 }
+
 
