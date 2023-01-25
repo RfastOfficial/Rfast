edc54777661374677c2047bde20028b76e5c2c7e
statlink <mtsagris@yahoo.gr>
2023-01-25 23:22:03 +0200

    change

--- R/multivariate_mle.R
+++ R/multivariate_mle.R
@@ -1,37 +1,39 @@
 #[export]
-invdir.mle <- function(x, tol = 1e-09) {
-
-  n <- dim(x)[1]
-  p <- dim(x)[2]
-  zx <- t( log(x) )
-  sx2 <- sum( log1p( Rfast::rowsums(x) ) )
-  com <- c( Rfast::rowsums(zx) - sx2, -sx2 )
-  a <- Rfast::colmeans(x)   
-  b <- Rfast::colVars(x)
-  D <- p + 1 
-  aD <- 0.5 * ( mean(a)^2 + mean(a) ) / mean(b) + 1
-  a1 <- abs( c( a * (aD - 1), aD) ) / 2
-  phi <- sum(a1)
-  f1 <- n * digamma( phi ) - n * digamma(a1) + com
-  f2 <- matrix(n * trigamma(phi), D, D)
-  diag(f2) <- diag(f2) - n * trigamma(a1)
-  a2 <- a1 - solve(f2, f1)
-  i <- 2
-  while ( sum( abs(a2 - a1) ) > tol ) {   
-    i <- i + 1
-    a1 <- a2
+invdir.mle <- function(x, tol = 1e-07) {
+    n <- dim(x)[1]
+    p <- dim(x)[2]
+    zx <- t( log(x) )
+    sx2 <- sum( log1p(Rfast::rowsums(x)) )
+    ep <- Rfast::rowsums(zx) 
+    com <- c(ep - sx2, -sx2)
+    ep <- sum(ep)
+    a <- Rfast::colmeans(x)
+    b <- Rfast::colVars(x)
+    D <- p + 1
+    aD <- 0.5 * ( mean(a)^2 + mean(a) ) / mean(b) + 1
+    a1 <- 0.5 * abs( c(a * (aD - 1), aD) ) 
     phi <- sum(a1)
-    f1 <- n * digamma( phi ) - n * digamma(a1) + com
+    lik1 <- n * lgamma(phi) - n * sum( lgamma(a1) ) + sum(zx * a1[1:p]) - phi * sx2
+    f1 <- n * digamma(phi) - n * digamma(a1) + com
     f2 <- matrix(n * trigamma(phi), D, D)
     diag(f2) <- diag(f2) - n * trigamma(a1)
     a2 <- a1 - solve(f2, f1)
-  }
-  
-  phi <- sum(a2)
-  lik <-  n * lgamma( phi ) - n * sum( lgamma(a2) ) +
-      sum( zx * (a2[1:p] - 1) ) - phi * sx2   
-  list(iters = i, loglik = lik, param = a2 )
-} 
+    phi <- sum(a2)
+    lik2 <- n * lgamma(phi) - n * sum( lgamma(a2) ) + sum(zx * a2[1:p]) - phi * sx2
+    i <- 2
+    while ( abs(lik2 - lik1) > tol) {
+        i <- i + 1
+        lik1 <- lik2
+        a1 <- a2
+        f1 <- n * digamma(phi) - n * digamma(a1) + com
+        f2 <- matrix(n * trigamma(phi), D, D)
+        diag(f2) <- diag(f2) - n * trigamma(a1)
+        a2 <- a1 - solve(f2, f1)
+        phi <- sum(a2)
+        lik2 <- n * lgamma(phi) - n * sum( lgamma(a2) ) + sum(zx * a2[1:p]) - phi * sx2
+    }
+    list(iters = i, loglik = lik2 - ep, param = a2)
+}
 
 
 #[export]
