f77be398067433798f3f771c262709bfd65cdefd
Manos Papadakis <papadakm95@gmail.com>
2023-06-07 15:56:30 +0300

    Update col_row_utilities_p.cpp

--- src/col_row_utilities_p.cpp
+++ src/col_row_utilities_p.cpp
@@ -256,61 +256,7 @@ BEGIN_RCPP
 END_RCPP
 }
 
-///////////////////////////////////////////////////////////////////////////
-
-
-NumericVector col_max_p(NumericMatrix x){
-  const int n=x.ncol();
-  mat xx(x.begin(),x.nrow(),n,false);
-  NumericVector f(n);
-  #ifdef _OPENMP
-    #pragma omp parallel for
-  #endif
-  for(int i=0;i<n;++i){
-    f[i]=*max_element(xx.begin_col(i),xx.end_col(i));
-  }
-  return f;
-}
-
-RcppExport SEXP Rfast_col_max_p(SEXP nSEXP){
-BEGIN_RCPP
-    RObject __result;
-    RNGScope __rngScope;
-    traits::input_parameter< NumericMatrix >::type n(nSEXP);
-    __result = col_max_p(n);
-    return __result;
-END_RCPP
-}
-
 //////////////////////////////////////////////////////////////
-
-using std::min_element;
-
-NumericVector col_min_p(NumericMatrix x){
-  const int n=x.ncol();
-  mat xx(x.begin(),x.nrow(),n,false);
-  NumericVector f(n);
-  #ifdef _OPENMP
-    #pragma omp parallel for
-  #endif
-  for(int i=0;i<n;++i){
-    f[i]=*min_element(xx.begin_col(i),xx.end_col(i));
-  }
-  return f;
-}
-
-RcppExport SEXP Rfast_col_min_p(SEXP nSEXP){
-BEGIN_RCPP
-    RObject __result;
-    RNGScope __rngScope;
-    traits::input_parameter< NumericMatrix >::type n(nSEXP);
-    __result = col_min_p(n);
-    return __result;
-END_RCPP
-}
-
-/////////////////////////////////////////////////////////////////////
-
 
 SEXP col_all_p(LogicalMatrix x){
   const int n=x.ncol();
