032a53dea41e774700d67c3e9f6adec0b48fb3a1
Manos Papadakis <papadakm95@gmail.com>
2023-06-07 15:58:20 +0300

    support data frame

--- R/min_max.R
+++ R/min_max.R
@@ -25,10 +25,8 @@ rowMaxs <- function(x,value=FALSE) {
 
 #[export]
 colMins <- function(x,value=FALSE,parallel = FALSE) {
-	if(parallel){
-		.Call(Rfast_col_min_p,x)	
-	}else if(value){
-		.Call(Rfast_col_min,x)
+	if(value){
+		.Call(Rfast_col_min,x,parallel)
 	}else{
     	.Call(Rfast_col_min_indices,x)
 	}
@@ -36,17 +34,15 @@ colMins <- function(x,value=FALSE,parallel = FALSE) {
 
 #[export]
 colMaxs <- function(x,value=FALSE,parallel = FALSE) {
-	if(parallel){
-		.Call(Rfast_col_max_p,x)	
-	}else if(value){
-		.Call(Rfast_col_max,x)
+	if(value){
+		.Call(Rfast_col_max,x,parallel)
 	}else{
     	.Call(Rfast_col_max_indices,x)
 	}
 }
 
 #[export]
-colMinsMaxs <- function(x) {
+colMinsMaxs <- function(x,parallel = FALSE) {
 	x <- .Call(Rfast_col_min_max,x)
 	rownames(x) <- c("min","max")
 	x
