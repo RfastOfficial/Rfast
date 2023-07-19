b22770d8f614ab22f21172f4ed042d6486b5b6e9
Manos Papadakis <papadakm95@gmail.com>
2023-07-19 15:33:27 +0300

    fix bug in bhattacharyya method

--- src/dists.cpp
+++ src/dists.cpp
@@ -313,7 +313,7 @@ NumericMatrix bhattacharyya_dist(NumericMatrix x)
     xv = xx.col(i);
     for (j = i + 1; j < ncl; ++j)
     {
-      a = sum(sqrt(xv * xx.col(j)));
+      a = sum(sqrt(xv % xx.col(j)));
       f(i, j) = a;
       f(j, i) = a;
     }
