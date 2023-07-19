e9a6715b383b33c37e4392ebd7f50239ea2fe67c
Manos Papadakis <papadakm95@gmail.com>
2023-07-19 23:58:24 +0300

    Update dista.R

--- R/dista.R
+++ R/dista.R
@@ -1,17 +1,9 @@
-#[export]
-dista <- function(xnew,x,type = "euclidean",k=0,index=FALSE,trans = TRUE,square = FALSE,p = 0,parallel = FALSE) {
-	if(type == "canberra1" || type == "canberra2"){
-		.Deprecated("The replacement type is \"canberra\"","Rfast")
-	}
-	if(k>0){
-		if(index){
-			x <- .Call(Rfast_dista_index,t(xnew),t(x),k,type)
-		}else{
-			x <- .Call(Rfast_dista_values,t(xnew),t(x),k,square,type)
-		}
-	}else{
-		x <- .Call(Rfast_dista,t(xnew),t(x),square,type,p,parallel)
-	}
-	if(trans)	x <- t(x)
-	x
+# [export]
+dista <- function(xnew, x, type = "euclidean", k = 0, index = FALSE, trans = TRUE, square = FALSE, p = 0, parallel = FALSE) {
+    if (type == "canberra1" || type == "canberra2") {
+        .Deprecated("The replacement type is \"canberra\"", "Rfast")
+    }
+    x <- .Call(Rfast_dista_index, t(xnew), t(x), type, square, p, k, index, parallel)
+    if (trans) x <- t(x)
+    x
 }
