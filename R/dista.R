6385fbe5a8baedac175349e3d5217a52e744c353
Manos Papadakis <papadakm95@gmail.com>
2023-07-19 22:32:58 +0300

    deprecated canberra 1 and 2 methods. Add parallel for some types

--- R/dista.R
+++ R/dista.R
@@ -1,5 +1,8 @@
 #[export]
 dista <- function(xnew,x,type = "euclidean",k=0,index=FALSE,trans = TRUE,square = FALSE,p = 0,parallel = FALSE) {
+	if(type == "canberra1" || type == "canberra2"){
+		.Deprecated("The replacement type is \"canberra\"","Rfast")
+	}
 	if(k>0){
 		if(index){
 			x <- .Call(Rfast_dista_index,t(xnew),t(x),k,type)
