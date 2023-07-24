4cb7a3b26d8c75aef81afc5b889fec8be62cc3be
Manos Papadakis <papadakm95@gmail.com>
2023-07-24 20:23:45 +0300

    Update Dist.h

--- src/Dist.h
+++ src/Dist.h
@@ -3,6 +3,12 @@
 #ifndef DIST_H
 #define DIST_H
 
+#include <Rcpp.h>
+#include <string>
+
+using Rcpp::NumericMatrix;
+using std::string;
+
 namespace Dist
 {
     NumericMatrix euclidean(NumericMatrix, const bool);
@@ -21,6 +27,6 @@ namespace DistaIndices
 {
 }
 
-double total_dista(NumericMatrix, NumericMatrix, const bool);
-
+double total_dista(NumericMatrix Xnew, NumericMatrix X, const string method = "",
+                          const bool sqr = false, const double p = 0.0, const unsigned int k = 0, const bool parallel = false);
 #endif
