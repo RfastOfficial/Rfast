ce4a8c66bf6be9e525ff00f860e5b31dbffec185
Manos Papadakis <papadakm95@gmail.com>
2023-09-18 13:49:30 +0300

    add parallel version

--- R/eachcol.apply.R
+++ R/eachcol.apply.R
@@ -1,4 +1,4 @@
 #[export]
-eachcol.apply<-function(x,y,indices = NULL,oper = "*",apply = "sum"){
-	.Call(Rfast_eachcol_apply,x,y,indices,oper,apply)
+eachcol.apply<-function(x,y,indices = NULL,oper = "*",apply = "sum", parallel = FALSE){
+	.Call(Rfast_eachcol_apply,x,y,indices,oper,apply, parallel)
 }
