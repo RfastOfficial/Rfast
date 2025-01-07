9c7983317e84a0e6e02cf7e4409411a2845c84d7
Michail Tsagris <mtsagris@yahoo.gr>
2025-01-07 11:42:35 +0200

    change

--- R/eqdist.etest.R
+++ R/eqdist.etest.R
@@ -7,29 +7,30 @@ eqdist.etest <- function(y, x, R = 999) {
     i <- 1:ni
     x <- Rfast::Sort(x)
     sx <- sum(x)
-    dii <- 2 * sum(i * x ) - (ni + 1) * sx
+    dii <- 2 * sum(i * x) - (ni + 1) * sx
     #djj is the sum of all pairwise distances of y
     nj <- length(y)
     j <- 1:nj
     y <- Rfast::Sort(y)
     sy <- sum(y)
     s <- sx + sy
-    djj <- 2 * sum(j * y ) - (nj + 1) * sy
+    djj <- 2 * sum(j * y) - (nj + 1) * sy
     n <- ni + nj  ## total sample size
-    dij <- Rfast::dista(x, y, result = "sum")
-    dtot <- dii + djj + dij
+    z <- Rfast::Sort( c(x, y) )
+    ##dij <- Rfast::dista(x, y, result = "sum")
+    dtot <- 2 * sum( ( 1:n ) * z ) - (n + 1) * s
+    dij <- dtot - dii - djj
     stat <- dij - nj * dii / ni - ni * djj / nj
 
-    z <- c(x, y)
     pstat <- numeric(R)
     for ( k in 1:R ) {
       id <- Rfast2::Sample.int(n, ni)
       xp <- Rfast::Sort(z[id])
       sxp <- sum(xp)
-      pdii <- 2 * sum(i * xp ) - (ni + 1) * sxp
+      pdii <- 2 * sum(i * xp) - (ni + 1) * sxp
       yp <- Rfast::Sort(z[-id])
       syp <- s - sxp
-      pdjj <- 2 * sum(j * yp ) - (nj + 1) * syp
+      pdjj <- 2 * sum(j * yp) - (nj + 1) * syp
       pdij <- dtot - pdii - pdjj  
       pstat[k] <- pdij - nj * pdii / ni - ni * pdjj / nj
     }
@@ -45,7 +46,7 @@ eqdist.etest <- function(y, x, R = 999) {
     }
   }
   ( sum( pstat >= stat ) + 1 ) / (R + 1)
-} 
+}  
   
 
 #total.vecdist <- function(x) {
