ee5ff3f6bbdd485f0d99a94d78b691b181461afc
Manos Papadakis <papadakm95@gmail.com>
2025-03-05 12:34:14 +0200

    fix bug about wrong results

--- R/group.R
+++ R/group.R
@@ -56,10 +56,10 @@ group.sum <- function(x, ina,ina.max = NULL,ina.min = NULL) {
 
 #[export]
 group<-function(x,ina,method="sum",ina.min=NULL,ina.max = NULL,
-	ina.length.unique=NULL,mad.method="median", std = FALSE, sorted = FALSE) {
+	ina.length.unique=NULL,mad.method="median", std = FALSE, sorted = TRUE) {
 	# if(method=="med"){
 	# 	ina.max<- if(is.null(ina.max)) max(ina) 
 	# 	ina.min<- if(is.null(ina.length.unique)) length(unique(ina))
 	# }
-	.Call(Rfast_group,x,ina,method,mad.method, sorted, std)
+	.Call(Rfast_group,x,ina,method,mad.method, std, sorted)
 }
