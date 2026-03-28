642f2712529cc63d4422f52447136a0139079f19
Iñaki Ucar <iucar@fedoraproject.org>
2026-03-28 11:40:51 +0100

    Add missing wrap

--- src/maha_ex.cpp
+++ src/maha_ex.cpp
@@ -20,6 +20,6 @@ RcppExport SEXP Rfast_mahaCpp(SEXP XSEXP, SEXP muSEXP, SEXP sigmaSEXP, SEXP isCh
 
     arma::mat X_(X.begin(), X.nrow(), X.ncol(), false), sigma_(sigma.begin(), sigma.nrow(), sigma.ncol(), false);
     arma::vec mu_(mu.begin(), mu.size(), false);
-	return mahaInt(X_, mu_, sigma_, isChol);
+	return wrap(mahaInt(X_, mu_, sigma_, isChol));
 	END_RCPP
 }
