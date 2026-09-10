d0393966afef4b203954083cccd0c14745e951cc
Manos Papadakis <papadakm95@gmail.com>
2026-09-10 14:22:06 +0300

    Update rvmf.cpp

--- src/rvmf.cpp
+++ src/rvmf.cpp
@@ -6,7 +6,8 @@
 #include <math.h>
 #include <zigg/header>
 #include "Rfast.h"
-#include "Random.h"
+//[[Rcpp::depends(rangen)]]
+#include <rangen.h>
 #ifdef _OPENMP
 #include <omp.h>
 #endif
@@ -14,62 +15,62 @@
 using namespace Rcpp;
 using namespace arma;
 
-static zigg::Ziggurat ziggurat;
-Random::uniform<Random::real> rng(0, 1);
+// static zigg::Ziggurat ziggurat;
+// Random::uniform<Random::real> rng(0, 1);
 
-class Gamma {
-	double shape, rate, d, c;
+// class Gamma {
+// 	double shape, rate, d, c;
 
-   public:
-	Gamma() {}
-	Gamma(double shape, double rate)
-		: shape(shape), rate(1.0 / rate), d(this->shape - 1.0 / 3.0), c(1.0 / std::sqrt(9.0 * d)) {}
+//    public:
+// 	Gamma() {}
+// 	Gamma(double shape, double rate)
+// 		: shape(shape), rate(1.0 / rate), d(this->shape - 1.0 / 3.0), c(1.0 / std::sqrt(9.0 * d)) {}
 
-	inline double operator()() {
-		// Marsaglia and Tsang method for rate >= 1
-		while (true) {
-			double x = ziggurat.rnorm(), x2 = x * x;
-			double v = 1.0 + c * x;
-			v = v * v * v;
-			double u = rng();
+// 	inline double operator()() {
+// 		// Marsaglia and Tsang method for rate >= 1
+// 		while (true) {
+// 			double x = rangen::norm(), x2 = x * x;
+// 			double v = 1.0 + c * x;
+// 			v = v * v * v;
+// 			double u = rangen::sunif();
 
-			if (v > 0 && (u < 1.0 - 0.0331 * x2 * x2 || std::log(u) < 0.5 * x2 + d * (1.0 - v + std::log(v)))) {
-				return d * v * rate;
-			}
-		}
-	}
-};
+// 			if (v > 0 && (u < 1.0 - 0.0331 * x2 * x2 || std::log(u) < 0.5 * x2 + d * (1.0 - v + std::log(v)))) {
+// 				return d * v * rate;
+// 			}
+// 		}
+// 	}
+// };
 
-class BetaOne {
-   protected:
-	double alpha;
-	Gamma alpha_d;
+// class BetaOne {
+//    protected:
+// 	double alpha;
+// 	Gamma alpha_d;
 
-   public:
-	BetaOne() {}
-	BetaOne(double alpha) : alpha(alpha), alpha_d(Gamma(alpha, 1.0)) {}
+//    public:
+// 	BetaOne() {}
+// 	BetaOne(double alpha) : alpha(alpha), alpha_d(Gamma(alpha, 1.0)) {}
 
-	inline double operator()() {
-		double x = alpha_d();
-		return x / (x + alpha_d());
-	}
-};
+// 	inline double operator()() {
+// 		double x = alpha_d();
+// 		return x / (x + alpha_d());
+// 	}
+// };
 
-class Beta : public BetaOne {
-	double beta;
-	Gamma beta_d;
+// class Beta : public BetaOne {
+// 	double beta;
+// 	Gamma beta_d;
 
-   public:
-	Beta(double alpha, double beta) : BetaOne(alpha), beta(beta), beta_d(Gamma(this->beta, 1.0)) {}
+//    public:
+// 	Beta(double alpha, double beta) : BetaOne(alpha), beta(beta), beta_d(Gamma(this->beta, 1.0)) {}
 
-	inline double operator()() {
-		double x = alpha_d();
-		return x / (x + beta_d());
-	}
-};
+// 	inline double operator()() {
+// 		double x = alpha_d();
+// 		return x / (x + beta_d());
+// 	}
+// };
 
-BetaOne *_beta;
-Random::uniform<Random::real> *_rng;
+rangen::BetaOne *_beta;
+rangen::rUniform *_rng;
 
 #ifdef _OPENMP
 #pragma omp threadprivate(_beta, _rng)
@@ -87,16 +88,17 @@ static mat rotation(colvec b) {
 	return A;
 }
 
-static void randn_z(mat &res, double mean = 0.0, double stddev = 1.0)
-{
-    for (auto &elem : res)
-    {
-        elem = ziggurat.rnorm();
-    }
-}
+// static void randn_z(mat &res, double mean = 0.0, double stddev = 1.0)
+// {
+//     for (auto &elem : res)
+//     {
+//         elem = rangen::norm();
+//     }
+// }
 
 static colvec rvmf_h(unsigned int n, double ca, double d1, double x0, double m, double k, double b,
-					 const bool parallel = false, const unsigned int cores = get_num_of_threads()) {
+					 const bool parallel = false, const unsigned int cores = get_num_of_threads(), 
+					 rangen::Seed *seed  = new rangen::Seed(rangen::get_cur_nano())) {
 	colvec w(n, fill::none);
 	const double bp1 = 1.0 + b, bm1 = 1.0 - b;
 
@@ -105,9 +107,10 @@ static colvec rvmf_h(unsigned int n, double ca, double d1, double x0, double m, 
 #pragma omp parallel num_threads(cores)
 #endif
 		{
-			_beta = new BetaOne(m);
-			_rng = new Random::uniform<Random::real>(0, 1);
-
+			_beta = new rangen::BetaOne(m);
+			_rng = new rangen::rUniform(0, 1);
+			_beta->setSeed(seed);
+			_rng->setSeed(seed);
 #ifdef _OPENMP
 #pragma omp for
 #endif
@@ -125,12 +128,14 @@ static colvec rvmf_h(unsigned int n, double ca, double d1, double x0, double m, 
 			delete _beta;
 		}
 	} else {
-		BetaOne beta(m);
+		rangen::BetaOne beta(m);
+		beta.setSeed(seed);
+		rangen::sunif.setSeed(seed);
 		for (unsigned int i = 0; i < n; ++i) {
 			double ta, u, z, tmp = 0;
 			for (ta = -1000.0, u = 1.0; ta - ca < log(u);) {
 				z = beta();
-				u = rng();
+				u = rangen::sunif();
 				tmp = (1.0 - bp1 * z) / (1.0 - bm1 * z);
 				ta = k * tmp + d1 * log1p(-x0 * tmp);
 			}
@@ -168,12 +173,14 @@ static colvec rvmf_h(unsigned int n, double ca, double d1, double x0, double m, 
 // 	}
 // }
 
-void rvmf(unsigned int n, colvec mu, double k, mat &out, const bool parallel, const unsigned int cores) {
+void rvmf(unsigned int n, colvec mu, double k, mat &out, const bool parallel, const unsigned int cores, 
+	rangen::Seed *seed  = new rangen::Seed(rangen::get_cur_nano())) {
+
 	if (k > 0.0) {
 		mu = mu / sqrt(sum(square(mu)));
 		const unsigned int d1 = mu.n_elem - 1;
 		mat S(n, d1 + 1, fill::none), S_h(S.begin(), n, d1, false);
-		randn_z(S_h);
+		rangen::fill::rnorm(S_h);	
 		double b = (-2.0 * k + sqrt(4.0 * k * k + d1 * d1)) / d1;
 		double x0 = (1.0 - b) / (1.0 + b);
 		double m = 0.5 * d1;
@@ -198,23 +205,23 @@ void rvmf(unsigned int n, colvec mu, double k, mat &out, const bool parallel, co
 				out = S * A.t();
 		}
 	} else {
-		randn_z(out);
+		rangen::fill::rnorm(out);	
 		out.each_col() /= sqrt(sum(square(out), 1));
 	}
 }
 
-NumericMatrix rvmf(unsigned int n, NumericVector Mu, double k, const bool parallel, const unsigned int cores) {
+NumericMatrix rvmf(unsigned int n, NumericVector Mu, double k, const bool parallel = false, const unsigned int cores = get_num_of_threads(), rangen::Seed *seed  = new rangen::Seed(rangen::get_cur_nano())) {
 	colvec mu(Mu.begin(), Mu.size(), false);
 	NumericMatrix Res(n, mu.n_elem);
 	mat res(Res.begin(), n, mu.n_elem, false);
-	rvmf(n, mu, k, res, parallel, cores);
+	rvmf(n, mu, k, res, parallel, cores, seed);
 	Nullable<CharacterVector> names = Mu.names();
 	if (names.isNotNull()) colnames(Res) = static_cast<CharacterVector>(Mu.names());
 	return Res;
 }
 
 template <class T>
-T rvonmises(unsigned int n, T m, double k, const bool rads, const bool parallel, const unsigned int cores) {
+T rvonmises(unsigned int n, T m, double k, const bool rads, const bool parallel = false, const unsigned int cores = get_num_of_threads(), rangen::Seed *seed  = new rangen::Seed(rangen::get_cur_nano())) {
 	colvec u(n, fill::none);
 	constexpr double pi = M_PI;
 	constexpr double pi_180 = pi / 180.0;
@@ -228,11 +235,11 @@ T rvonmises(unsigned int n, T m, double k, const bool rads, const bool parallel,
 	mu(span(m.n_elem, mu.n_elem - 1)) = sin(m);
 	if (k > 0) {
 		mat x(n, mu.n_elem, fill::none);
-		rvmf(n, mu, k, x, parallel, cores);
+		rvmf(n, mu, k, x, parallel, cores, seed);
 		u = (atan(x.col(1) / x.col(0)) + pi * conv_to<colvec>::from(x.col(0) < 0));
 		u = u.for_each([&](double &v) { return v - std::floor(v / pi_2) * pi_2; });
 	} else {
-		u = randu<colvec>(n, distr_param(0.0, pi_2));
+		rangen::fill::runif(u, 0.0, pi_2);
 	}
 	if (!rads) {
 		u *= pi_180;
@@ -241,10 +248,10 @@ T rvonmises(unsigned int n, T m, double k, const bool rads, const bool parallel,
 }
 
 template <>
-NumericVector rvonmises<NumericVector>(unsigned int n, NumericVector m, double k, const bool rads, const bool parallel, const unsigned int cores) {
+NumericVector rvonmises<NumericVector>(unsigned int n, NumericVector m, double k, const bool rads, const bool parallel, const unsigned int cores, rangen::Seed *seed) {
 	NumericVector Res(n);
 	colvec res(Res.begin(), n, false), M(m.begin(), m.size(), false);
-	colvec tmp = rvonmises<colvec>(n, m, k, rads, parallel, cores);
+	colvec tmp = rvonmises<colvec>(n, m, k, rads, parallel, cores, seed);
 	res = tmp;
 	return Res;
 }
@@ -261,7 +268,7 @@ NumericVector rvonmises<NumericVector>(unsigned int n, NumericVector m, double k
 //	return F;
 //}
 
-RcppExport SEXP Rfast_rvmf(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP parallelSEXP, SEXP coresSEXP) {
+RcppExport SEXP Rfast_rvmf(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP parallelSEXP, SEXP coresSEXP, SEXP seedSEXP) {
 	BEGIN_RCPP
 	RObject __result;
 	RNGScope __rngScope;
@@ -270,12 +277,13 @@ RcppExport SEXP Rfast_rvmf(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP parallelSEXP
 	traits::input_parameter<double>::type k(kSEXP);
 	traits::input_parameter<const bool>::type parallel(parallelSEXP);
 	traits::input_parameter<const unsigned int>::type cores(coresSEXP);
-	__result = rvmf(n, m, k, parallel, cores);
+	Rcpp::XPtr<rangen::Seed> seed(seedSEXP);
+	__result = rvmf(n, m, k, parallel, cores, seed);
 	return __result;
 	END_RCPP
 }
 
-RcppExport SEXP Rfast_rvonmises(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP radsSEXP, SEXP parallelSEXP, SEXP coresSEXP) {
+RcppExport SEXP Rfast_rvonmises(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP radsSEXP, SEXP parallelSEXP, SEXP coresSEXP, SEXP seedSEXP) {
 	BEGIN_RCPP
 	RObject __result;
 	RNGScope __rngScope;
@@ -285,8 +293,8 @@ RcppExport SEXP Rfast_rvonmises(SEXP nSEXP, SEXP mSEXP, SEXP kSEXP, SEXP radsSEX
 	traits::input_parameter<const bool>::type rads(radsSEXP);
 	traits::input_parameter<const bool>::type parallel(parallelSEXP);
 	traits::input_parameter<const unsigned int>::type cores(coresSEXP);
-
-	__result = rvonmises<NumericVector>(n, m, k, rads, parallel, cores);
+	Rcpp::XPtr<rangen::Seed> seed(seedSEXP);
+	__result = rvonmises<NumericVector>(n, m, k, rads, parallel, cores, seed);
 	return __result;
 	END_RCPP
 }
